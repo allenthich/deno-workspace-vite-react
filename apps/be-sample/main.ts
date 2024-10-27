2771941525589116baad80ae4341492063c81159
Allen Thich <allenthich@gmail.com>
2024-10-27 18:41:38 +0900

    Add example import

--- apps/be-sample/main.ts
+++ apps/be-sample/main.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
+import { addPetMutationKey } from "@libs/pets";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text(`Hello Hono! ${JSON.stringify(addPetMutationKey())}}`);
 });
 
 Deno.serve(app.fetch);
