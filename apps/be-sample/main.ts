82e59363bdc38d5081e244ae2692bce30976a8d2
Batama <thebatama@gmail.com>
2024-10-28 16:18:13 +0900

    unified imports, tasks in from root

--- apps/be-sample/main.ts
+++ apps/be-sample/main.ts
@@ -1,9 +1,9 @@
-import { Hono } from "hono";
+import { Hono } from "@hono/hono";
 import { addPetMutationKey } from "@libs/pets";
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", (c: { text: (arg0: string) => any }) => {
   return c.text(`Hello Hono! ${JSON.stringify(addPetMutationKey())}}`);
 });
 
--- apps/fe-sample/vite.config.ts
+++ apps/fe-sample/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import deno from "@deno/vite-plugin";
+// import tsconfigPaths from "npm:vite-tsconfig-paths";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react(), tsconfigPaths()],
+  plugins: [deno(), react(), tsconfigPaths({ ignoreConfigErrors: true })],
   cacheDir: "../../node_modules/.vite",
 });
