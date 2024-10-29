5a9e813b8658b54dedc74558ff8a174bd0a645ba
Batama <thebatama@gmail.com>
2024-10-29 15:14:39 +0900

    replace swagger-ui-dist to @hono/swagger-ui

--- apps/swagger/main.ts
+++ apps/swagger/main.ts
@@ -1,6 +1,6 @@
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
-import * as swaggerUiDist from "swagger-ui-dist";
+import { swaggerUI } from "@hono/swagger-ui";
 import { parse } from "jsr:@std/yaml";
 import * as path from "jsr:@std/path";
 
@@ -22,37 +22,10 @@ const apiSpec = parse(
 ) as object;
 
 app.use(serveStatic({ root: "./public/" }));
-app.use(
-  "/api-docs/*",
-  serveStatic({ root: swaggerUiDist.getAbsoluteFSPath() }),
-);
+app.get("/api-docs", swaggerUI({ url: "/openapi.json" }));
 // Serve the OpenAPI YAML as JSON for Swagger UI
 app.get("/openapi.json", (c) => {
   return c.json(apiSpec);
-});
-app.get("/api-docs", (c) => {
-  const swaggerHtml = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>API Docs</title>
-      <link rel="stylesheet" href="/css/swagger-ui.css" />
-    </head>
-    <body>
-      <div id="swagger-ui"></div>
-      <script src="/js/swagger-ui-bundle.js"></script>
-      <script>
-        const ui = SwaggerUIBundle({
-          url: '/openapi.json',
-          dom_id: '#swagger-ui',
-        });
-      </script>
-    </body>
-    </html>
-  `;
-  return c.html(swaggerHtml);
 });
 
 Deno.serve({ port: 8081 }, app.fetch);
